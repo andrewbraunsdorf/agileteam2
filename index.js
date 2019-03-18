0d17172f4eb3bf5757695d214edc63e80bdcf3d4
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-18 23:33:29 +0000

    added api.js

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(req, res){
-	console.log('GET requests');
-	res.send({name: 'name'});
-});
+app.use('/api', require("/api"));
 
-app.post('/', function(req, res){
-	console.log('POST requests');
-	res.send({name: 'name'});
-});
 
+// set up to listen for requests
 app.listen(process.env.port || 8080, function(){
 	console.log('now listening for requests');
 })
